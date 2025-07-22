c97148141d3c838a0fe43ccab7ffac2262470332
Damien <damien@aeromac.com>
2025-07-22 22:27:07 +1000

    Updated interface

--- src/main/java/dev/reuise/webstyles/StyleSheet.java
+++ src/main/java/dev/reuise/webstyles/StyleSheet.java
@@ -1,26 +1,26 @@
 package dev.reuise.webstyles;
 import dev.reuise.core.State;
 import java.util.Map;
-public interface StyleSheet extends Style {
+public interface StyleSheet {
     StyleSheet getAtRule(String atRule);
 
-    StyleSheet getRule(String rule, State state);
+    Style getRule(String rule, State state);
 
-    void setRule(String rule, StyleSheet style);
+    void setRule(String rule, Style style);
 
     void setAtRule(String atRule, StyleSheet styleSheet);
 
     void appendRule(String rule, Style style);
 
-    Map<String, StyleSheet> getRules();
+    Map<String, Style> getRules();
 
     Map<String, StyleSheet> getAtRules();
 
     String getStateQuery();
 
-    StyleSheet addRule(String rule, State state);
+    Style addRule(String rule, State state);
 
-    StyleSheet addRule(String rule);
+    Style addRule(String rule);
 
     StyleSheet addAtRule(String rule, State state);
 
